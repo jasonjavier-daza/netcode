5ad30cd8b77bfc5856f5417102d28f9f60a2d731
jasonjavier-daza <jasonjavier.daza@hotmail.com>
2021-04-13 20:01:23 -0500

    ajustes en home y navbar

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -7,20 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
   private activeAnt: any;
+  public scrollOffset: number = 0;
+  public logo: string = '../../../assets/img/Logo-netcode-blanco.png';
   constructor() {}
 
   ngOnInit(): void {}
   over(event: MouseEvent) {
     console.log(event);
     const target = event.target as HTMLButtonElement;
-    if(this.activeAnt && this.activeAnt != target){
+    if (this.activeAnt && this.activeAnt != target) {
       console.log(this.activeAnt);
       this.activeAnt.classList.remove('active');
-
     }
     this.activeAnt = target;
     target.classList.add('active');
 
     console.log(event);
+  }
+  @HostListener('window:scroll', ['$event'])
+  private handleScroll(event: Event) {
+    console.log(event);
+    const target = event.target as HTMLButtonElement;
+    this.scrollOffset = target.children[0].scrollTop;
+    if (this.scrollOffset > 80) {
+      this.logo = '../../../assets/img/Logo-netcode-verde.png';
+    }else{
+      this.logo = '../../../assets/img/Logo-netcode-blanco.png';
+    }
+    console.log('window scroll: ', this.scrollOffset);
   }
 }
