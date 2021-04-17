05b8dcf7b3435d80951fbf4a864b1b9e7d49a1f2
jasonjavier-daza <jasonjavier.daza@hotmail.com>
2021-04-17 12:53:42 -0500

    ajustes en secciones del home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,17 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
   title = 'netcode';
-  parentMessage = "message from parent";
-  constructor() { }
+  public proyectos: number = 0;
+  public miembros: number = 0;
+  public jefes: number = 0;
+  public clientes: number = 0;
+  public scrollOffset: number = 0;
+  public counter: object = {
+    proyectos: 78,
+    miembros: 23,
+    jefes: 7,
+    clientes: 374,
+  };
+  parentMessage = 'message from parent';
+  constructor() {}
 
-  ngOnInit(): void {
-    
+  ngOnInit(): void {}
+  @HostListener('window:scroll', ['$event'])
+  private handleScroll(event: Event) {
+    var self = this;
+    console.log(event);
+    const target = event.target as HTMLButtonElement;
+    this.scrollOffset = target.children[0].scrollTop;
+    if (this.scrollOffset > 1764) {
+      let limit = 0;
+      let intercount = setInterval(function () {
+        if (self.counter['proyectos'] > self.proyectos) {
+          self.proyectos++;
+        }else if(self.counter['proyectos'] > self.proyectos){
+          limit++;
+        }
+        if (self.counter['miembros'] > self.miembros) {
+          self.miembros++;
+        }else if(self.counter['miembros'] > self.miembros){
+          limit++;
+        }
+        if (self.counter['jefes'] > self.jefes) {
+          self.jefes++;
+        }else if(self.counter['jefes'] > self.jefes){
+          limit++;
+        }
+        if (self.counter['clientes'] > self.clientes) {
+          self.clientes++;
+        }else if(self.counter['clientes'] == self.clientes){
+          limit++;
+        }
+        if (limit == 4) {
+          clearInterval(intercount);
+        }
+        console.log("entra");
+      }, 100);
+    }
+    console.log('window scroll2: ', this.scrollOffset);
   }
-
 }
